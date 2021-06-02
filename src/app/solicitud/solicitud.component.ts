875e0c961be7cb471c407783cbf907b9f684e36a
alvarocalvo@gmail.com <JitJub2020>
2021-06-02 13:18:06 +0200

    EJERCICIO 2

---
+++ src/app/solicitud/solicitud.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-solicitud',
+  templateUrl: './solicitud.component.html',
+  styleUrls: ['./solicitud.component.css']
+})
+export class SolicitudComponent implements OnInit {
+
+  solicitud = {nombre: "Alvaro"}
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
